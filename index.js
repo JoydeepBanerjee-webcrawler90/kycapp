9f71b84218c8c04914b803ef2ae4c69a20f3b73a
Joydeep <joydeep@shyamfuture.com>
2021-05-09 02:25:57 +0530

    Fixes

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ db.mongoose
 // }
 
 app.use(express.static('client/build'));
-    app.get('/', (req,res) => {
+    app.get('/*', (req,res) => {
       console.log('intoit');
       res.sendFile(path.resolve(__dirname,'client','build','index.html'));
     })
