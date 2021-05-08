ee5f33f5b2475c7a939822743b0b96dc34a07d70
Joydeep <joydeep@shyamfuture.com>
2021-05-09 01:22:25 +0530

    node js ini path fix

--- index.js
+++ index.js
@@ -53,8 +53,13 @@ require('./routes/upload.routes')(app);
 //       res.sendFile(path.resolve(__dirname,'client','build','index.html'));
 //     })
 // }
-app.use(express.static(path.join(__dirname, "client/build")));
-app.get("/*", (req, res) => res.sendFile(path.join(__dirname, "./index.html")));
+
+app.use(express.static('client/build'));
+    app.get('*', (req,res) => {
+      res.sendFile(path.resolve(__dirname,'client','build','index.html'));
+    })
+// app.use(express.static(path.join(__dirname, "client/build")));
+// app.get("/*", (req, res) => res.sendFile(path.join(__dirname, "/index.html")));
 
 
 // set port, listen for requests
