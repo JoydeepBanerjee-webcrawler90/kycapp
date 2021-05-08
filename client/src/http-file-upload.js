e860e301313e1db4734d8772bf5fda6debbd2914
Joydeep <joydeep@shyamfuture.com>
2021-05-09 02:16:45 +0530

    Fixes

--- client/src/http-file-upload.js
+++ client/src/http-file-upload.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 const token = localStorage.getItem('access_token');
 export default axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: "https://shyamkycdemo.herokuapp.com/api",
   headers: {
     "Content-type": "multipart/form-data",
     "x-access-token":token
